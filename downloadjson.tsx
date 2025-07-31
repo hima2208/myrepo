b8299420f5c7765c34c21df8efe025b1d5f5cb32
hima2208 <39065769+hima2208@users.noreply.github.com>
2025-07-31 16:13:40 -0400

    Create downloadjson.tsx

---
+++ downloadjson.tsx
@@ -0,0 +1,8 @@
+const downloadJSON = (data: object, filename: string = 'environment-request.json') => {
+  const blob = new Blob([JSON.stringify(data, null, 2)], { type: 'application/json' });
+  const link = document.createElement('a');
+  link.href = URL.createObjectURL(blob);
+  link.download = filename;
+  link.click();
+  URL.revokeObjectURL(link.href);
+};
